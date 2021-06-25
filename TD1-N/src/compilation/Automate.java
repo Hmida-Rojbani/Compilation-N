1a2a612722ab4121935fb83412d55fedd8846e10
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-06-25 20:04:56 +0100

    update

--- TD1-N/src/compilation/Automate.java
+++ TD1-N/src/compilation/Automate.java
@@ -1,30 +1,115 @@
 package compilation;
 
 public class Automate {
-	
+
 	public static boolean onlyOneOccurence(String s) {
-		char c ;
+		char c;
 		int etat = 0;
-		int i=0;
-		OUTER: while(i < s.length()) {
+		int i = 0;
+		OUTER: while (i < s.length()) {
 			c = s.charAt(i);
-			if(c!='a' && c!='b')
+			if (c != 'a' && c != 'b')
 				throw new IllegalArgumentException("Wrong alphabet");
 			switch (etat) {
-			case 0: if(c=='b')
-					etat=1;				
+			case 0:
+				if (c == 'b')
+					etat = 1;
 				break;
-			case 1: if(c=='b')
-				etat=2;				
-			break;
+			case 1:
+				if (c == 'b')
+					etat = 2;
+				break;
 
 			case 2:
 				break OUTER;
 			}
 			i++;
 		}
-		
-		return etat==1;
+
+		return etat == 1;
+	}
+
+	public static boolean validInteger(String s) {
+		char c;
+		int etat = 0;
+		int i = 0;
+		OUTER: while (i < s.length()) {
+			c = s.charAt(i);
+			if (c != '+' && c != '-' && !Character.isDigit(c))
+				throw new IllegalArgumentException("Wrong alphabet");
+			switch (etat) {
+			case 0:
+				if (c == '+' || c == '-')
+					etat = 1;
+				else
+					etat = 2;
+				break;
+			case 1:
+				if (Character.isDigit(c))
+					etat = 2;
+				else
+					etat = 3;
+				break;
+
+			case 2:
+				if (!Character.isDigit(c))
+					etat = 3;
+				break;
+			case 3:
+				break OUTER;
+			}
+			i++;
+		}
+
+		return etat == 2;
+	}
+
+	public static boolean validRational(String s) {
+		char c;
+		int etat = 0;
+		int i = 0;
+		OUTER: while (i < s.length()) {
+			c = s.charAt(i);
+			if (c != '+' && c != '-' && c != '/' && !Character.isDigit(c))
+				throw new IllegalArgumentException("Wrong alphabet");
+			switch (etat) {
+			case 0:
+				if (c == '+' || c == '-')
+					etat = 1;
+				else if(Character.isDigit(c))
+					etat = 2;
+				else 
+					etat =5;
+				break;
+			case 1:
+				if (Character.isDigit(c))
+					etat = 2;
+				else
+					etat = 5;
+				break;
+
+			case 2:
+				if (c == '/')
+					etat = 3;
+				else if (!Character.isDigit(c))
+					etat = 5;
+				break;
+			case 3:
+				if (Character.isDigit(c))
+					etat = 4;
+				else
+					etat = 5;
+			case 4:
+				if (!Character.isDigit(c))
+					etat = 5;
+				break;
+			case 5:
+				break OUTER;
+			}
+			i++;
+		}
+
+		return etat == 4;
 	}
 
 }
